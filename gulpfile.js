2b3abd86a02dd4bcc1d47c439991e69c30fb751a
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-10-20 15:01:04 +0800

    fs

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
+	express = require('express'),
 	exec = require('child_process').exec,
-	cmd = 'git add .;git commit -m  "ff" ;git push lgd master';
-gulp.task('watch',function(){
-	var server = livereload();
-	gulp.watch('./*', function (file) {
-        exec(cmd,function(err,stdout,stderr){
+	cmd = 'git pull lgd master;pm2 restart /Users/liuchang/project/git/lgd/app.js',
+	app = express();
+	app.get('/',function(req,res){
+		exec(cmd,function(err,stdout,stderr){
         	if(err){
         		console.log('error');
         	}else{
         		console.log(stdout);
         	}
-        })
-    });
-});	
+        });
+		res.send('success');
+	});
+	app.listen(3100);
+// gulp.task('watch',function(){
+// 	var server = livereload();
+// 	gulp.watch('./*', function (file) {
+//         exec(cmd,function(err,stdout,stderr){
+//         	if(err){
+//         		console.log('error');
+//         	}else{
+//         		console.log(stdout);
+//         	}
+//         })
+//     });
+// });	
