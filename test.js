b581b7111056c5f8e5803f7a1e940ec2c8296218
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-10-21 15:21:54 +0800

    fsa

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 var request = require('request');
-var rsync = require('req-sync');
-
-request('http://2.uanli.sinaapp.com/api/?controller=grade&studentId=2015&action=read',function(err,res,body){
-	// console.log(body);
-	body = JSON.parse(body);
-	console.log(body.data[0]);
+var httpSync = require('http-sync-4');
+var request = httpSync.request({
+	method:'GET',
+	host:'2.uanli.sinaapp.com',
+	path:'/api/?controller=grade&studentId=2015&action=read'
 });
+var response = request.end();
+
+console.log(JSON.parse(response.body.toString("utf-8")).data[0]);
+// request('http://2.uanli.sinaapp.com/api/?controller=grade&studentId=2015&action=read',function(err,res,body){
+// 	// console.log(body);
+// 	body = JSON.parse(body);
+// 	console.log(body.data[0]);
+// });
